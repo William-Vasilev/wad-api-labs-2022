051e01c596f24e2f06aa220ef084dfbefe09cf1b
williamvasilev <williamvasilev@gmail.comgit config --global user.name William>
2022-11-26 20:07:18 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,9 +1,10 @@
+import './db';
+//import './seedData';
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
-import './db';
-import './seedData'
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ app.use(express.json());
 app.use('/api/movies', moviesRouter);
 
 app.use('/api/genres', genresRouter);
+
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
