5fdbe708cd0db91b069f0769a79f0f930bc44591
williamvasilev <williamvasilev@gmail.comgit config --global user.name William>
2022-11-28 14:35:41 +0000

    Topic 10 - Session based Auth

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,8 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
 import usersRouter from './api/users';
+import session from 'express-session';
+import authenticate from './authenticate';
 
 dotenv.config();
 
@@ -19,11 +21,19 @@ const errHandler = (err, req, res, next) => {
 };
 
 
+
 const app = express();
 
 const port = process.env.PORT;
 
 app.use(express.json());
+
+//session middleware
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}));
 
 
 app.use('/api/movies', moviesRouter);
@@ -31,6 +41,8 @@ app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
 
 app.use('/api/users', usersRouter);
+
+app.use('/api/movies', authenticate, moviesRouter);
 
 app.use(errHandler);
 
